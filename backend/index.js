d6df3bcd83109603322b0e1f7fef565144e42ae4
Sahil <sk2003311@gmail.com>
2024-02-20 22:22:40 +0530

    done with env

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
 
+const app = express();
 
+app.get("/", (req, res) => {
+    res.json({
+        message: "Helo live"
+    })
+})
+
+app.listen(8000, () => {
+    console.log("listenning to port 8000")
+})
