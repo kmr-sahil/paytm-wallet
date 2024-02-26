dec9f20253e88eb5cd33f06bcdb8a7ba1759fbda
Sahil <sk2003311@gmail.com>
2024-02-26 17:59:31 +0530

    test

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div className='text-black text-3xl'>
-        jgsdkg ejkhdfkjh
+        <Link to={"/signin"}>Sign In</Link>
     </div>
   )
 }
